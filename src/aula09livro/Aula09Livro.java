d4561cdd104e28d1caf210259cdd574443d56b54
eduardodea85 <eduardodea@outlook.com>
2023-03-01 01:02:59 -0300

    Aula-09-livros

---
+++ src/aula09livro/Aula09Livro.java
@@ -0,0 +1,12 @@
+
+package aula09livro;
+
+
+public class Aula09Livro {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/aula09livro/Publicacao.java
@@ -0,0 +1,10 @@
+
+package aula09livro;
+
+public interface Publicacao {
+    public void abrir();
+    public void fechar();
+    public void folhear();
+    public void avancarPag();
+    public void voltarPag();    
+}
